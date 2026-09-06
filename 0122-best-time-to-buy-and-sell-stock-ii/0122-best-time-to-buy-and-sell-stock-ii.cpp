ba1c7fae47205253b38f3c3939bfde675e26e567
VickyChoudhary12 <vickychoudhary7011@gmail.com>
2026-09-06 22:44:58 +0530

    Time: 0 ms (100%), Space: 19.8 MB (97.28%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,9 +4,9 @@ public:
         int n=prices.size();
         int sum=0;
         for(int i=1;i<n;i++){
-           if(prices[i]>prices[i-1]){
-            sum+=prices[i]-prices[i-1];
-           }
+            if(prices[i]>prices[i-1]){
+                sum+=prices[i]-prices[i-1];
+            }
         }
         return sum;
     }
