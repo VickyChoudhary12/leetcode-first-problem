485810f61020d20c168c383918172829a1ef7ae0
VickyChoudhary12 <vickychoudhary7011@gmail.com>
2026-09-06 23:43:21 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.29%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -8,7 +8,6 @@ public:
         for(int i=0;i<n;i++){
             if(pre==0) pre=1;
             if(suff==0) suff=1;
-
             pre*=nums[i];
             suff*=nums[n-i-1];
             ans=max(ans,max(pre,suff));
